1cb8f8e5250dd6808b68a68b740f7f4f5a51ef53
wendellunny <wendelferreiraportugal@gmail.com>
2021-10-16 22:44:12 -0300

    Criando a estrutura de dados do fogo

---
+++ assets/js/fire.js
@@ -0,0 +1,26 @@
+const firePixelsArray = [];
+const fireWidth = 2;
+const fireHeight = 3;
+
+function start(){
+    createFireDataStructure();
+    console.log(firePixelsArray);
+}
+
+function createFireDataStructure(){
+    const numberOfPixels = fireWidth * fireHeight;
+    for (i=0; i < numberOfPixels; i++){
+        firePixelsArray[i] = 0;
+    }
+
+}
+
+function createFirePropagation(){
+
+}
+
+function createRenderFire(){
+
+}
+start();
+
